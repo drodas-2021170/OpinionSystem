3d964834eef1c3df95c4c4d0aef1e5c173c94679
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-02-23 00:07:29 -0600

    Se cambia de lugar el llamado de la funcion defaultAdmin

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import { initServer } from "./configs/app.js"
 import { config } from "dotenv"
 import { connect } from "./configs/mongo.js"
 import { defaultCategory } from "./src/category/category.controller.js"
+import { defaultAdmin } from './src/auth/auth.controller.js'
 
 config()
 initServer()
 connect()
 defaultCategory()
+defaultAdmin()
