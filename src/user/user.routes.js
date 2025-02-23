9a00cd55aafff8fe2329dbd193febfbeaf937899
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-02-23 14:57:57 -0600

    Se crea la ruta para obtener los usuarios, solo los admins pueden obtener todos los usuarios

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express"
-import { updateUser } from "./user.controller.js"
-import { validateJwt } from "../../middleware/validate.jwt.js"
+import { getUsers, updateUser } from "./user.controller.js"
+import { isAdmin, validateJwt } from "../../middleware/validate.jwt.js"
 import { updateUserValidate } from "../../middleware/validate.js"
 
 const api = Router()
 
 //Private Routes
-
+api.get('/', [validateJwt, isAdmin], getUsers)
 api.put('/updateUser',[validateJwt, updateUserValidate], updateUser)
 export default api
